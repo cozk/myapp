0a34301cc3f02216c14d68aaa1b9e74716e9c067
cozk <157584848@qq.com>
2018-06-04 11:11:33 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+
 var router = express.Router();
+
 var path = require('path');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+console.log(2222)
+
+
 });
+
 module.exports = router;
